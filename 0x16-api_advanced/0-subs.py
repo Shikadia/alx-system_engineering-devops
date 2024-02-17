b248e6eb684eebfa8623aafd9127799c1d9708b5
Shikadia <133326472+Shikadia@users.noreply.github.com>
2024-02-18 00:28:39 +0100

    Fix subs file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,16 +10,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # Check if response is successful
-    if response.status_code == 200:
-        # Try to access JSON data, handle if subreddit doesn't exist
-        try:
-            results = response.json().get("data")
-            return results.get("subscribers")
-        except AttributeError:
-            # Subreddit doesn't exist or JSON response is malformed
-            return 0
-    else:
-        # Subreddit doesn't exist or other error occurred
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
